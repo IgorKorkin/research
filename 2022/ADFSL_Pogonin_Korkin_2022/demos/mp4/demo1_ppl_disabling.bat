3e0cbaa7974f2200d0fa997105ef8fb08fde2392
Igor Korkin <igor.korkin@gmail.com>
2023-01-02 11:52:23 +0300

    Add files from 2022

---
+++ 2022/ADFSL_Pogonin_Korkin_2022/demos/mp4/demo1_ppl_disabling.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\VideoLAN\VLC\vlc.exe" --no-playlist-autostart --qt-continue=Never --fullscreen --no-video-title-show --play-and-pause "C:\cdisk_camtasia\2022\ADFSL\output\demo1_patch_integrity.mp4" --volume 512
+
+REM --no-playlist-autostart
---
+++ 2022/ADFSL_Pogonin_Korkin_2022/demos/mp4/demo2_patch_integrity_revoke_privs.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\VideoLAN\VLC\vlc.exe" --no-playlist-autostart --qt-continue=Never --fullscreen --no-video-title-show --play-and-pause "C:\cdisk_camtasia\2022\ADFSL\output\demo2_patch_integrity_revoke_privs.mp4" --volume 512
+
+REM --no-playlist-autostart
